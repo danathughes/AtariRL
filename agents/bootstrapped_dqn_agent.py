3dc0f7960142c19fdbba0a49e5427047bf9cc97c
Dana Hughes <danathughes@gmail.com>
2017-09-20 00:05:50 -0600

    Bootstrapped DQN

---
+++ agents/bootstrapped_dqn_agent.py
@@ -0,0 +1,188 @@
+## bootstrapped_dqn_agent.py
+##
+## Agent which implements a Bootstrapped Deep Q Network as a control policy
+##
+
+import numpy as np
+import os
+
+import scipy.ndimage as ndimage
+
+import tensorflow as tf
+from models.BootstrappedDeepQNetwork import *
+# from models.DuelingDeepQNetwork import *
+
+from listeners.tensorboard_monitor import *
+
+class Bootstrapped_DQN_Agent:
+	"""
+	Agent which implements a Bootstrapped DQN to learn a policy
+	"""
+
+	def __init__(self, input_shape, shared_layers, head_layers, num_actions, num_heads, replay_memory, counter, config, **kwargs):
+		"""
+		action_update_rate - number of frames to repeat an action
+		"""
+
+		self.num_actions = num_actions
+		self.num_heads = num_heads
+
+		# Which frame / step are we on 
+		self.counter = counter
+
+		# Need to query the replay memory for training examples
+		self.replay_memory = replay_memory
+		self.replay_start_size = kwargs.get('replay_start_size', 5000)
+
+		# Discount factor, etc.
+		self.discount_factor = kwargs.get('discount_factor', 0.99)
+		self.minibatch_size = kwargs.get('minibatch_size', 32)
+
+		# Count the actions to determine action repeats and update frequencies
+		self.update_frequency = kwargs.get('update_frequency', 4)
+
+		# Should the network train?
+		if self.counter.count >= self.replay_start_size:
+			self.can_train = True
+			print "DQN can train..."
+		else:
+			self.can_train = False
+
+		# Keep track of frames to know when to train, switch networks, etc.
+		self.target_update_frequency = kwargs.get('target_update_frequency', 1000)
+
+		# Did the user provide a session?
+		self.sess = kwargs.get('tf_session', tf.InteractiveSession())
+
+		# Initialize a Tensorflow session and create two DQNs
+		self.dqn = BootstrappedDeepQNetwork(input_shape, shared_layers, head_layers, num_actions, self.num_heads, self.sess, network_name='dqn')
+		self.target_dqn = BootstrappedDeepQNetwork(input_shape, shared_layers, head_layers, num_actions, self.num_heads, self.sess, network_name='target_dqn', trainable=False)
+
+		self.update_operation = UpdateOperation(self.dqn, self.target_dqn, self.sess)
+
+		# Maintain a history of the previous states for use as input
+		self.state_history = np.zeros((84,84,4))
+
+		self.listeners = []
+
+
+	def add_listener(self, listener):
+		"""
+		"""
+
+		self.listeners.append(listener)
+
+
+
+	def start_episode(self):
+		"""
+		"""
+
+		self.head_number = np.random.randint(self.num_heads)		
+
+
+	def observe(self, state):
+		"""
+		Receive an observation
+		"""
+
+		self.state_history[:,:,0:3] = self.state_history[:,:,1:4]
+		self.state_history[:,:,3] = state
+
+
+	def act(self):
+		"""
+		Select an action based on the current state history
+		"""
+
+		# Select an action
+		Q = self.dqn.get_Qs(self.state_history)
+		action = np.argmax(Q)
+
+		return action, Q
+
+
+	def learn(self, action, reward, is_terminal):
+		"""
+		Learn from the action taken (maybe due to environmental influence, etc), provided reward and next state
+		"""
+
+		# Add the experience to the replay memory
+		self.replay_memory.record(self.state_history[:,:,3], action, reward, is_terminal)		
+
+		# Has enough frames occured to start training?
+		if self.counter.count == self.replay_start_size:
+			self.can_train = True
+			print "Start Training..."
+
+		# Should training occur?  
+		if self.counter.count % self.update_frequency == 0 and self.can_train:
+			self.train()
+
+		# Should the target network be updated?
+		if self.counter.count % self.target_update_frequency == 0:
+			self.update_target_network()
+
+
+	def createDataset(self, size):
+		"""
+		"""
+
+		# Create and populate arrays for the input, target and mask for the DQN
+		experiences, indices, weights = self.replay_memory.get_samples(32)
+		states, actions, rewards, next_states, terminals = experiences
+		masks = self.replay_memory.get_masks(indices)
+
+		# Get what the normal output would be for the DQN
+		targets = self.dqn.get_Qs(states)
+
+		target_Q = np.zeros((size,))
+
+		# Update the Q value of only the action
+		Qmax = np.max(self.target_dqn.get_Qs(next_states), axis=1)
+		Qnext = (1.0 - terminals.astype(np.float32)) * self.discount_factor * Qmax
+
+		idx = np.arange(size)
+
+		targets[idx,actions] = rewards[idx] + Qnext[idx]
+
+		target_Q = rewards + Qnext
+
+		# Calculate the TD error and inform the memory, for possible update
+		TD_error = target_Q - self.dqn.get_Qs(states)[idx, actions]
+		self.replay_memory.update(indices, TD_error)
+
+		return states, targets, actions, target_Q, weights, masks
+
+
+	def update_target_network(self):
+		"""
+		Copy the currently trained DQN into the target network
+		"""
+
+		print "Updating Target DQN..."
+		
+		self.update_operation.run()
+
+
+	def train(self):
+		"""
+		Train the network
+		"""
+
+		# Get the training data
+		inputs, targets, actions, target_Q, weights = self.createDataset(self.minibatch_size)
+		data = {'input': inputs, 'target': target_Q, 'action': actions, 'weights': weights}
+
+		# Train the network
+		loss = self.dqn.train(data)
+		Qs = self.dqn.get_Qs(inputs)
+
+		# Summarize the Q values and training loss
+		training_data = {'training_loss': loss}
+		for i in range(self.num_actions):
+			training_data['Q%d_training' % i] = Qs[:,i]
+
+		for listener in self.listeners:
+			listener.record(training_data)
+		
---
+++ memory/bootstrapped_memory.py
@@ -0,0 +1,83 @@
+import numpy as np
+import os
+
+import cPickle as pickle
+
+import scipy.ndimage as ndimage
+from memory import ReplayMemory
+
+class BootstrappedReplayMemory:
+	"""
+	"""
+
+	def __init__(self, memory_size, base_memory, num_Q_functions):
+		"""
+		Create a recorder to record the dataset
+		"""
+
+		self.memory_size = memory_size
+
+		# The current index of the buffer.  Assume a circular buffer
+		self._idx = 0
+		self.filled = False		# Has the buffer been filled?
+
+		# Build an actual memory buffer
+		self.memory = base_memory
+
+		# Mask for which Q function(s) should learn from this 
+		self.masks = np.zeros((memory_size, num_Q_functions))
+
+
+	def record(self, frame, action, reward, is_terminal):
+		"""
+		Store this state, action and reward.  Flush and start a new batch if necessary
+		"""
+
+		self.memory.record(frame, action, reward, is_terminal)
+
+		# Generate a mask
+		self.masks[self._idx] = np.random.binomial(1, 0.5, (10,))
+
+		# Syncronize the indices, etc.
+		self._idx = self.memory._idx
+		self.filled = self.memory.filled
+
+
+	def update(self, indices, TD_error):
+		"""
+		Update the priorities at the indices with the new TD error
+		"""
+
+		self.memory.update(indices, TD_error)
+
+
+	def get_samples(self, size, history_length=4):
+		"""
+		Return an array of samples
+		"""
+
+		experiences, indices, weights = self.memory.get_samples(size, history_length)
+
+		return experiences, indices, weights, self.masks[indices]
+
+
+	def get_masks(self, indices):
+		"""
+		Return the masks recorded at the given indices
+		"""
+
+		return self.masks[indices]
+
+
+	def save(self, path):
+		"""
+		"""
+
+		self.memory.save(path)
+
+
+	def load(self, path):
+		"""
+		"""
+
+		self.memory.load(path)
